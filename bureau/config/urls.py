a90f4ac9a5dde1a324c57c2110ca008fdb05d5cf
Max Brauer <max.brauer@picturepipe.com>
2018-02-16 22:37:55 +0100

    Changed url config to macht django 2.0

--- bureau/config/urls.py
+++ bureau/config/urls.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
-from django.conf.urls import include, url
+from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^grappelli/', include('grappelli.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
