8062cd13f864b2bc42e71c81f1e9dc4a6f66c483
Max Brauer <max.brauer@picturepipe.com>
2018-02-16 23:15:15 +0100

    core: added basic (and empty) index page

--- bureau/config/urls.py
+++ bureau/config/urls.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
-from django.conf.urls import url
+from django.urls import include, path
 from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', include('bureau.apps.core.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
