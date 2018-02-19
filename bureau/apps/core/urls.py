ab5cc4b634e28915d3c2606d474f61c21f6d2652
Max Brauer <max.brauer@picturepipe.com>
2018-02-19 21:13:14 +0100

    Added kitchensink page.

--- bureau/apps/core/urls.py
+++ bureau/apps/core/urls.py
@@ -3,4 +3,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='core/index.html'), name='index'),
+    path('kitchensink', TemplateView.as_view(template_name='core/kitchensink.html'), name='kitchensink'),
 ]
