828521823f9c746b3c2ae3dd2f6bfedb47e5c358
Max Brauer <max.brauer@picturepipe.com>
2018-02-19 21:27:12 +0100

    Fixed testsuite.

--- bureau/apps/core/urls.py
+++ bureau/apps/core/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='core/index.html'), name='index'),
-    path('kitchensink', TemplateView.as_view(template_name='core/kitchensink.html'), name='kitchensink'),
+    path(
+        '',
+        TemplateView.as_view(template_name='core/index.html'),
+        name='index'
+    ),
+    path(
+        'kitchensink',
+        TemplateView.as_view(template_name='core/kitchensink.html'),
+        name='kitchensink'
+    ),
 ]
