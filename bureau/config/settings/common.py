457ea64247414d35e61f247f3f882131f68ae75b
Max Brauer <max.brauer@picturepipe.com>
2018-02-19 09:59:25 +0100

    Add sass-processor to installed apps and node_modules to STATICFILES_DIRS

--- bureau/config/settings/common.py
+++ bureau/config/settings/common.py
@@ -120,11 +120,13 @@ class Common(Configuration):
         # Always use forward slashes, even on Windows.
         # Don't forget to use absolute paths, not relative paths.
         os.path.join(BaseDir.BASE_DIR, 'static'),
+        os.path.join(os.path.dirname(BaseDir.BASE_DIR), 'node_modules', 'normalize.css'),
     )
 
     STATICFILES_FINDERS = values.ListValue([
         'django.contrib.staticfiles.finders.AppDirectoriesFinder',
         'django.contrib.staticfiles.finders.FileSystemFinder',
+        'sass_processor.finders.CssFinder',
         #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
     ])
 
@@ -184,6 +186,8 @@ class Common(Configuration):
         'django.contrib.staticfiles',
         'django.contrib.admin',
         'django.contrib.admindocs',
+
+        'sass_processor',
 
         'bureau.apps.core.apps.CoreConfig',
     )
